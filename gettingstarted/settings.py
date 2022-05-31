fdbff139ee18ce128d62d88a37953185a005e8ee
Sahib Hernández <warmarines00@hotmail.com>
2022-05-31 17:37:01 -0500

    update secure changes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     	'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd5u5reg1e2cfoe',
         'USER': 'scpdcehjmfiywd',
-        'PASSWORD': '3db48870df5950039ba47a8dddb9631ba7c6ad62f472c49939a792528ebb3446',
+        'PASSWORD': 'SECRET_PASSWORD', #If required, contact: warmarines00@hotmail.com
         'HOST': 'ec2-54-157-79-121.compute-1.amazonaws.com',
         'PORT': '5432',
         #"ENGINE" : "django.db.backends.sqlite3",
